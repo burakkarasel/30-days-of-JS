7c34bfa4ed1878bf1cf166a2d80401dae02b0daa
kullop <burakcankarasel@gmail.com>
2022-05-31 11:29:14 +0300

    Day02 project completed including script.js file

---
+++ day02/script.js
@@ -0,0 +1,24 @@
+const hour = document.querySelector(".hour")
+
+const min = document.querySelector(".min")
+
+const sec = document.querySelector(".sec")
+
+
+const setDate = function(){
+    const now = new Date()
+    const seconds = now.getSeconds()
+
+    sec.style.transform = `rotate(${(seconds / 60)*360 + 90}deg)`
+
+    const mins = now.getMinutes()
+
+    min.style.transform = `rotate(${(mins / 60)*360 + 90}deg)`
+
+    const hours = now.getHours()
+
+    hour.style.transform = `rotate(${(hours / 12)*360 + 90}deg)`
+}
+
+setInterval(setDate, 1000)
+
